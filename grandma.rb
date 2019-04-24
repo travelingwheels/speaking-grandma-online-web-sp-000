b22c73d1a23f4956f470cafa82f4160cfd2c9187
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-24 00:08:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,9 +17,10 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
     
     elsif phrase == phrase.upcase
-    return"NO, NOT SINCE 1938!"
+      return"NO, NOT SINCE 1938!"
     
-    
+    else
+      return "HUH?! SPEAK UP, SONNY!"
     
   end
 end
